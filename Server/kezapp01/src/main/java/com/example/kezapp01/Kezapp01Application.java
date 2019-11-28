d7e68622ff7eb8f57e42a156edce0addf9d0e7a6
faberdin <faberdin@gmail.com>
2019-11-28 15:27:25 +0100

    server 01

---
+++ Server/kezapp01/src/main/java/com/example/kezapp01/Kezapp01Application.java
@@ -0,0 +1,13 @@
+package com.example.kezapp01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kezapp01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kezapp01Application.class, args);
+	}
+
+}
---
+++ Server/kezapp01/src/test/java/com/example/kezapp01/Kezapp01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kezapp01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kezapp01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

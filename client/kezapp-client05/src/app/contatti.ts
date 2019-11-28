8ec2368bed7088e5059eb606a2155d2a790cc589
Erikman61 <Erikman61@gmail.com>
2019-11-28 18:07:45 +0100

    Salvataggio Client-Server kezapp05

---
+++ client/kezapp-client05/src/app/contatti.ts
@@ -0,0 +1,12 @@
+export class Chat {
+    id: string;
+    nickname: string;
+    sessione: string;
+}
+
+export class Messaggio {
+    id: string;
+    Test: string;
+    aliasDestinatario: string;
+    aliasMittente: string;
+}

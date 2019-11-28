edb72720f813f4ed05489967ffd28da4c542d15d
GoDolphins <corradini.l@gmail.com>
2019-11-28 15:19:08 +0100

    Inserito Server07

---
+++ Server/kezapp07/src/main/java/com/example/kezapp07/Kezapp07Application.java
@@ -0,0 +1,13 @@
+package com.example.kezapp07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kezapp07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kezapp07Application.class, args);
+	}
+
+}
---
+++ Server/kezapp07/src/test/java/com/example/kezapp07/Kezapp07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kezapp07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kezapp07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

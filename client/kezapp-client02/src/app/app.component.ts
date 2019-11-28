b0a674d2b2bdd866a9d8f2fb32f915bc81ac45f8
abrocco <abrocco@tiscali.it>
2019-11-28 18:04:15 +0100

    commit finale

--- client/kezapp-client02/src/app/app.component.ts
+++ client/kezapp-client02/src/app/app.component.ts
@@ -1,4 +1,11 @@
+import { Chat, Messaggio } from './contatti';
 import { Component } from '@angular/core';
+
+import { RichiediRegistrazioneDto } from './richiedi-registrazione-dto';
+import { Observable } from 'rxjs';
+import { RegistrazioneDto } from './registrazione.-dto';
+import { HttpClient } from '@angular/common/http';
+import { InviaMessaggioDto } from './invia-messaggio-dto';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'kezapp-client02';
+  nickname: string = "";
+  messaggio: string = "";
+  contatti: Chat[] = [];
+  messaggi: Messaggio[] = [];
+  sessione: string = "";
+
+  readonly url: string = "http://localhost:8080";
+  readonly postfix: string = "02";
+
+  constructor(private http: HttpClient) { };
+
+  registrazione() {
+    let dto = new RichiediRegistrazioneDto();
+    dto.nickname = this.nickname;
+
+    // prepara la richiesta http
+    let ox: Observable<RegistrazioneDto> =
+      this.http.post<RegistrazioneDto>(
+        this.url + "registrazione" + this.postfix,
+        dto
+      );
+
+    // invoca il servizio Web
+    ox.subscribe(
+      r => {
+        this.contatti = r.contatti;
+        this.messaggi = r.messaggi;
+        this.sessione = r.sessione;
+
+      }
+
+    );
+  }
+
+  InviaTutti() {
+    let dto = new InviaMessaggioDto();
+    dto.messaggio = this.messaggio;
+    dto.sessione = this.sessione;
+    dto.destinatario = null;
+
+    // prepara la richiesta http
+    let ox: Observable<RegistrazioneDto> =
+      this.http.post<RegistrazioneDto>(
+        this.url + "invia-tutti" + this.postfix,
+        dto
+      );
+
+    // invoca il servizio Web
+    ox.subscribe(
+      r => {
+        this.contatti = r.contatti;
+        this.messaggi = r.messaggi;
+      }
+    );
+  }
 }

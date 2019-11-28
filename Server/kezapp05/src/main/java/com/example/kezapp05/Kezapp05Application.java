4c023c29a511469d137efb2cbb4216a3cc2fcc77
Erikman61 <Erikman61@gmail.com>
2019-11-28 15:13:29 +0100

    Server kezapp05

---
+++ Server/kezapp05/src/main/java/com/example/kezapp05/Kezapp05Application.java
@@ -0,0 +1,13 @@
+package com.example.kezapp05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kezapp05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kezapp05Application.class, args);
+	}
+
+}

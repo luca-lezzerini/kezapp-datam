e4df67ee20647b88d4c41bf50267c871f484b089
Luca Lezzerini <luca@lezzerini.it>
2019-11-28 15:16:00 +0100

    Creato server side

---
+++ Server/kezapp00/src/main/java/com/example/kezapp00/Kezapp00Application.java
@@ -0,0 +1,13 @@
+package com.example.kezapp00;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kezapp00Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kezapp00Application.class, args);
+	}
+
+}
---
+++ Server/kezapp00/src/test/java/com/example/kezapp00/Kezapp00ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kezapp00;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kezapp00ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

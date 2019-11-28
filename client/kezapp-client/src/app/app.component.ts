39e8f9628cb3faaf081520f660233328f8a24346
Luca Lezzerini <luca@lezzerini.it>
2019-11-28 17:30:17 +0100

    Chiamata WS registrazione e ngIf sulle due sezioni iniziali

--- client/kezapp-client/src/app/app.component.ts
+++ client/kezapp-client/src/app/app.component.ts
@@ -1,6 +1,9 @@
+import { RichiediRegistrazioneDto } from './richiedi-registrazione-dto';
 import { RegistrazioneDto } from './registrazione-dto';
 import { Chat, Messaggio } from './contatti';
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -11,16 +14,35 @@ import { Component } from '@angular/core';
 export class AppComponent {
   nickname: string = "";
   messaggio: string = "";
-  contatti:Chat[] = [];
-  messaggi:Messaggio[] = [];
+  contatti: Chat[] = [];
+  messaggi: Messaggio[] = [];
+  sessione: string = "";
 
-  readonly url:string ="http://localhost:8080/";
+  readonly url: string = "http://localhost:8080/";
   readonly postfix: string = "00";
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient) { }
 
-  registrazione(){
+  registrazione() {
+    // preparo le informazioni da mandare al server
+    let dto = new RichiediRegistrazioneDto();
+    dto.nickname = this.nickname;
+
+    // prepara la richiesta http
     let ox: Observable<RegistrazioneDto> =
+      this.http.post<RegistrazioneDto>(
+        this.url + "registrazione" + this.postfix,
+        dto
+      );
+
+      // invoca il servizio Web
+      ox.subscribe(
+        r =>{
+          this.contatti = r.contatti;
+          this.messaggi = r.messaggi;
+          this.sessione = r.sessione;
+        }
+      );
 
   }
 }

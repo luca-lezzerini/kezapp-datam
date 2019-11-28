a8e56b21ad39cc1e1f7aaaacd62480e13bbf81b2
Luca Lezzerini <luca@lezzerini.it>
2019-11-28 16:46:26 +0100

    Raffinamento client 00

--- client/kezapp-client/src/app/app.component.ts
+++ client/kezapp-client/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Chat, Messaggio } from './contatti';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'kezapp-client';
+  nickname: string = "";
+  messaggio: string = "";
+  contatti:Chat[] = [];
+  messaggi:Messaggio[] = [];
 }

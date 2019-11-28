a4c1fec498ab7fc66c7bf905a0291ccee0117bc9
abrocco <abrocco@tiscali.it>
2019-11-28 15:20:53 +0100

    committo tutto dopo il pull

---
+++ Server/kezapp02/src/test/java/com/example/kezapp02/Kezapp02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kezapp02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kezapp02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

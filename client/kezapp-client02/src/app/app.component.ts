1298b58fd992438e7df0147dccb8436a1cc7754c
abrocco <abrocco@tiscali.it>
2019-12-18 16:29:34 +0100

    commit

--- client/kezapp-client02/src/app/app.component.ts
+++ client/kezapp-client02/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Observable } from 'rxjs';
 import { RegistrazioneDto } from './registrazione.-dto';
 import { HttpClient } from '@angular/common/http';
 import { InviaMessaggioDto } from './invia-messaggio-dto';
+import { RichiedimessaggioDto } from './RichiediMessaggi';
 
 @Component({
   selector: 'app-root',
@@ -19,7 +20,7 @@ export class AppComponent {
   messaggi: Messaggio[] = [];
   sessione: string = "";
 
-  readonly url: string = "http://localhost:8080/";
+  readonly url: string = "http://84.22.108.21:8080/";
   readonly postfix: string = "02";
 
   constructor(private http: HttpClient) { };
@@ -68,4 +69,48 @@ export class AppComponent {
       }
     );
   }
+
+  invia(cx: Chat) {
+    let dto = new InviaMessaggioDto();
+    dto.messaggio = this.messaggio;
+    dto.sessione = this.sessione;
+    dto.destinatario =cx.nickname;
+
+    // prepara la richiesta http
+    let ox: Observable<RegistrazioneDto> =
+      this.http.post<RegistrazioneDto>(
+        this.url + "invia-uno" + this.postfix,
+        dto
+      );
+
+    // invoca il servizio Web
+    ox.subscribe(
+      r => {
+        this.contatti = r.contatti;
+        this.messaggi = r.messaggi;
+      }
+    );
+  }
+
+  aggiorna() {
+    let dto = new RichiedimessaggioDto();
+    dto.sessione = this.sessione;
+    
+
+    // prepara la richiesta http
+    let ox: Observable<RegistrazioneDto> =
+      this.http.post<RegistrazioneDto>(
+        this.url + "aggiorna" + this.postfix,
+        dto
+      );
+
+    // invoca il servizio Web
+    ox.subscribe(
+      r => {
+        this.contatti = r.contatti;
+        this.messaggi = r.messaggi;
+      }
+    );
+  }
+
 }
